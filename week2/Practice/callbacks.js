6fcae3f1d364ada1959e8edfe489439c0051dc23
Chase <chasehillig@yahoo.com>
2021-09-25 09:44:24 -0600

    forgot to add callback.js to be committed

---
+++ week2/Practice/callbacks.js
@@ -0,0 +1,19 @@
+// example 1:
+const lyrics = "lalalala"
+
+// the function we will callback to
+const displaySong = (lyrics) => {
+  console.log(lyrics)
+}
+
+// the function that takes in a callback function
+const sing = (lyrics, callback) => {
+  callback(lyrics)
+}
+
+// sing takes in a callback function (displaySong) and calls it to display the lyrics
+sing(lyrics, displaySong)
+
+// example 2: use an anonymous function
+const singAnonymous = (lyrics, () => console.log(lyrics))
+singAnonymous()
