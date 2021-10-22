beae1c97149aa694e329a70636f1366255c33cd5
Chase <chasehillig@yahoo.com>
2021-10-22 16:08:02 -0600

    frontend mostly finished

--- week6/scripts/test.js
+++ week6/scripts/test.js
@@ -1,7 +1,8 @@
 const checkboxes = document.querySelectorAll(".checkbox")
+const todoList = document.querySelector(".todo-container #todo-list")
 
-checkboxes.forEach((checkbox) => {
-  if (checkbox.checked) {
-    console.log("hello World")
-  }
-})
+// checkboxes.forEach((checkbox) => {
+//   if (checkbox.checked) {
+//     console.log("hello World")
+//   }
+// })
