001f326f38242b49f77b1e656ac363c151086857
Chase <chasehillig@yahoo.com>
2021-11-20 22:01:36 -0700

    added frontend form validation before sending data to server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,21 @@
 // connects to pokedex
 const path = require("path")
 const express = require("express")
+const cors = require("cors")
 const { engine } = require("express-handlebars")
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"], // can only allow users to make get and post request
+  })
+)
+
+app.use(express.json())
+app.use(express.urlencoded())
 
 const publicDirPath = path.join(__dirname, "../../pokedex/public") // static js and css files
 const viewsPath = path.join(__dirname, "../templates/views") // res.render() looks in view folder by default
@@ -28,12 +39,22 @@ app.get("", (req, res) => {
   res.render("index")
 })
 
-app.get("/about", (req, res) => {
-  res.render("about")
+app.get("/details", (req, res) => {
+  res.render("details")
 })
 
 app.get("/register", (req, res) => {
   res.render("register")
+})
+
+app.post("/register", (req, res) => {
+  console.log(req.body.firstName)
+})
+
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Hello World",
+  })
 })
 
 app.listen(port, () => {
---
+++ week10/app.js
@@ -0,0 +1,44 @@
+// can use npm modules thanks to browserify (refer to build script in package.json)
+const validator = require("validator")
+const registerForm = document.querySelector("#regForm")
+
+registerForm.addEventListener("submit", (e) => {
+  let isEmail = validateEmail(registerForm["email"].value)
+  let isStrongPassword = validatePassword(registerForm["password"].value)
+
+  if (isEmail && isStrongPassword) {
+    registerForm.submit()
+  } else {
+    alert("Password is not good")
+    e.preventDefault()
+  }
+})
+
+function validateEmail(email) {
+  return validator.isEmail(email)
+}
+
+function validatePassword(password) {
+  return validator.isStrongPassword(password, {
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+}
+
+// async function getDitto() {
+//   await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((error) => {
+//       console.log("didn't work")
+//     })
+// }
+
+// async function test() {
+//   const response = await fetch("http://127.0.0.1:3000/test")
+//   const { message } = await response.json()
+//   console.log(message)
+// }
